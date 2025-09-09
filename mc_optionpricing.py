6d7cdb5396c698d09e2eeff5ee1a072f5c06f0d7
Frans Englich <fenglich@fastmail.fm>
2025-09-09 14:59:59 +0200

    Tidy.

--- mc_optionpricing.py
+++ mc_optionpricing.py
@@ -1,35 +1,33 @@
 import numpy as np
-# import matplotlib.pyplot as plt
-# from numpy.random import randn
-
-"""
-A Monte Carlo pricing of an option.
-
-Fixed:
-    * Discount rate is risk-free, r = 0.05
-    * 2 time periods, t = 0 and t = 1
-    * Strike price = 38
-    * Underlying asset price, S_t:
-        - t=0: 40
-        - t=1: 48 or 30
-
-    * 2 possible states (?)
-
-Doesn't matter:
-    * Probabilities of movements of underlying asset.
-
-Assumptions:
-    * S_t is lognormal distributed: S_t ~ LN(μ, σ)
-
-We simulate:
-    * ?
-
-Some of the material used:
-* https://intro.quantecon.org/monte_carlo.html
-"""
 
 
-def main() -> None:
+def monte_carlo_pricing() -> None:
+    """
+    A Monte Carlo pricing of an option.
+
+    Fixed:
+        * Discount rate is risk-free, r = 0.05
+        * 2 time periods, t = 0 and t = 1
+        * Strike price = 38
+        * Underlying asset price, S_t:
+            - t=0: 40
+            - t=1: 48 or 30
+
+        * 2 possible states (?)
+
+    Doesn't matter:
+        * Probabilities of movements of underlying asset.
+
+    Assumptions:
+        * S_t is lognormal distributed: S_t ~ LN(μ, σ)
+
+    We simulate:
+        * ?
+
+    Some of the material used:
+    * https://intro.quantecon.org/monte_carlo.html
+    """
+
     # The asset's mean
     μ = 1.0
 
@@ -58,6 +56,10 @@ def main() -> None:
     P = β ** n * np.mean(return_draws)
 
     print(f"The Monte Carlo option price is approximately {P:3f}")
+
+
+def main() -> None:
+    monte_carlo_pricing()
 
 
 if __name__ == "__main__":
