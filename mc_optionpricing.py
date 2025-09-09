9a354f17fbc675f34da4d0bf7f1b1d4bb6c57a2b
Frans Englich <fenglich@fastmail.fm>
2025-09-09 12:47:22 +0200

    Clean file.

---
+++ mc_optionpricing.py
@@ -0,0 +1,19 @@
+
+def main() -> None:
+    # Let's run on the example in:
+    # https://www.youtube.com/watch?v=AukJ1gDeErw
+
+    # The correct values should be:
+    # up state: option value 10
+    # down state: option value 0
+    # stock amount: 10/18 = 0.55
+    # option price: 6.35
+
+    # 2. We compute our result
+    pass
+
+    # The computed values matches the example on Youtube.
+
+
+if __name__ == "__main__":
+    main()
