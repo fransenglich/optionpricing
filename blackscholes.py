987435c1aa1428b870b3280c122c42bde12802cc
Frans Englich <fenglich@fastmail.fm>
2025-09-10 13:45:07 +0200

    Adjust.

--- blackscholes.py
+++ blackscholes.py
@@ -7,18 +7,17 @@ def black_scholes(asset_price: float,
                   risk_free: float,
                   volatility: float) -> float:
     """An implementation of the Black-Scholes-Merton (BSM) equation for option
-    pricing."""
+    pricing.
 
-    sd_s = volatility
-    sd_v = volatility
+    An extensive source is John C. Hull (2022). Options, Futures and Other Derivatives."""
 
     # Dividing the equation into the parts d1, d2, and C is customary for BSM
 
-    d1 = (np.emath.logn(strike_price, asset_price) +
-        (risk_free + sd_v ** 2 / 2) * time_expiration) \
-        / (sd_s * np.sqrt(time_expiration))
+    d1 = np.emath.logn(strike_price, asset_price) + \
+        ((risk_free + volatility ** 2 / 2) * time_expiration) \
+        / (volatility * np.sqrt(time_expiration))
 
-    d2 = d1 - sd_s * np.sqrt(time_expiration)
+    d2 = d1 - volatility * np.sqrt(time_expiration)
 
     rng = np.random.default_rng()
     rng.random()
@@ -34,6 +33,8 @@ def main() -> None:
 
     # We run https://www.mystockoptions.com/black-scholes.cfm
     # which for the below values returns 7.366.
+    # This calculator yields the same (7.37):
+    # https://www.omnicalculator.com/finance/black-scholes
     mso = black_scholes(14, 10, 3.5, 0.05, 0.5)
 
     print(f"mso: {mso}")
